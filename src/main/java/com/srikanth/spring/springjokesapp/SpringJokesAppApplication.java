ce602d9ebd7025699777bdbda45b9610d7bd9200
springframework-joy <srikanth.josyula@gmail.com>
2018-09-08 10:01:32 -0400

    added banner.txt

--- src/main/java/com/srikanth/spring/springjokesapp/SpringJokesAppApplication.java
+++ src/main/java/com/srikanth/spring/springjokesapp/SpringJokesAppApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJokesAppApplication {
+public class
+SpringJokesAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJokesAppApplication.class, args);
