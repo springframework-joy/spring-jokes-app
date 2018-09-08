a62f2cf0b3799a4480871ca5d3c42c5c0bcdca0b
springframework-joy <srikanth.josyula@gmail.com>
2018-09-08 09:44:21 -0400

    Controller and Service Added

--- src/main/java/com/srikanth/spring/springjokesapp/controller/JokesController.java
+++ src/main/java/com/srikanth/spring/springjokesapp/controller/JokesController.java
@@ -1,9 +1,25 @@
 package com.srikanth.spring.springjokesapp.controller;
 
+import com.srikanth.spring.springjokesapp.services.QuotesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
-    public
+    private QuotesService quotesService;
+
+    @Autowired
+    public JokesController(QuotesService quotesService) {
+        this.quotesService = quotesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", quotesService.getRandomQuote());
+
+        return "chucknorris";
+    }
 }
--- src/main/java/com/srikanth/spring/springjokesapp/services/QuotesServiceImpl.java
+++ src/main/java/com/srikanth/spring/springjokesapp/services/QuotesServiceImpl.java
@@ -1,9 +1,19 @@
 package com.srikanth.spring.springjokesapp.services;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuotesServiceImpl implements QuotesService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public QuotesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
 
     @Override
     public String getRandomQuote() {
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
